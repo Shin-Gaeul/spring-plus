dac2c5bf29f24895f4bcf1c41ff4ea63afdfd2b7
t2023-m0014 <qa9377@gmail.com>
2025-01-23 19:59:54 +0900

    Level 1 읽기전용 상태 제거

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional // readOnly=ture 제거
 public class TodoService {
 
     private final TodoRepository todoRepository;
